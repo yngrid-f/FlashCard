e049e359acb81217efe0ad4864c02e66335c0c07
yngrid-f <140665786+yngrid-f@users.noreply.github.com>
2024-11-25 23:21:43 +0000

    atual

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Programação',
+    'O é Python?',
+    'O Python é uma linguagem de programação.'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Programação',
+    'O é Python?',
+    'O Python é uma linguagem de programação.'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
